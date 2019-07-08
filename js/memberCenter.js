8b3b5d9fe86b7b87a024a7a587f0ca634d0ebbd7
ak190527 <kungpop1107@gmail.com>
2019-07-08 13:56:47 +0800

    more

--- js/memberCenter.js
+++ js/memberCenter.js
@@ -1,58 +1,70 @@
 // 手機版下方 記錄區
-$(window).onload(funciton(){
+
+$( document ).ready(function(){
+
+
     $("#msgRecordBtn").click(function () {
         $("#msgRecord").toggle();
         $("#grpRecord").hide();
         $("#shopRecord").hide();
     });
-
+    
     $("#grpRecordBtn").click(function () {
         $("#msgRecord").hide();
         $("#grpRecord").toggle();
         $("#shopRecord").hide();
     });
-
+    
     $("#shopRecordBtn").click(function () {
         $("#msgRecord").hide();
         $("#grpRecord").hide();
         $("#shopRecord").toggle();
     });
-});
 
 
-// 桌機版 記錄區頁籤
-$('#msgLi').click(function () {
-    $('#msgRecord_web').show();
-    $('#grpRecord_web').hide();
-    $('#shopRecord_web').hide();
-    $('#msgLi').removeClass('white');
-    $('#msgLi').addClass('yellow');
-    $('#grpLi').removeClass('yellow');
-    $('#grpLi').addClass('white');
-    $('#shopLi').removeClass('yellow');
-    $('#shopLi').addClass('white');
-});
+    //桌機版 基本資訊欄_點擊按鈕換編輯欄
+    
 
-$('#grpLi').click(function () {
-    $('#msgRecord_web').hide();
-    $('#grpRecord_web').show();
-    $('#shopRecord_web').hide();
-    $('#msgLi').removeClass('yellow');
-    $('#msgLi').addClass('white');
-    $('#grpLi').removeClass('white');
-    $('#grpLi').addClass('yellow');
-    $('#shopLi').removeClass('yellow');
-    $('#shopLi').addClass('white');
-});
 
-$('#shopLi').click(function () {
-    $('#msgRecord_web').hide();
-    $('#grpRecord_web').hide();
-    $('#shopRecord_web').show();
-    $('#msgLi').removeClass('yellow');
-    $('#msgLi').addClass('white');
-    $('#grpLi').removeClass('yellow');
-    $('#grpLi').addClass('white');
-    $('#shopLi').removeClass('white');
-    $('#shopLi').addClass('yellow');
-});
+    // 桌機版 記錄區頁籤
+    $('#msgLi').click(function () {
+        $('#msgRecord_web').show();
+        $('#grpRecord_web').hide();
+        $('#shopRecord_web').hide();
+        $('#msgLi').removeClass('white');
+        $('#msgLi').addClass('yellow');
+        $('#grpLi').removeClass('yellow');
+        $('#grpLi').addClass('white');
+        $('#shopLi').removeClass('yellow');
+        $('#shopLi').addClass('white');
+    });
+
+    $('#grpLi').click(function () {
+        $('#msgRecord_web').hide();
+        $('#grpRecord_web').show();
+        $('#shopRecord_web').hide();
+        $('#msgLi').removeClass('yellow');
+        $('#msgLi').addClass('white');
+        $('#grpLi').removeClass('white');
+        $('#grpLi').addClass('yellow');
+        $('#shopLi').removeClass('yellow');
+        $('#shopLi').addClass('white');
+    });
+
+    $('#shopLi').click(function () {
+        $('#msgRecord_web').hide();
+        $('#grpRecord_web').hide();
+        $('#shopRecord_web').show();
+        $('#msgLi').removeClass('yellow');
+        $('#msgLi').addClass('white');
+        $('#grpLi').removeClass('yellow');
+        $('#grpLi').addClass('white');
+        $('#shopLi').removeClass('white');
+        $('#shopLi').addClass('yellow');
+    });
+
+})
+
+
+
+
