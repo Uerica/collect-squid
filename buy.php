d4250769d5a04c77414ceabb6754c138b4d1ccf2
theHardest <abcmattttt@gmail.com>
2019-07-19 09:57:34 +0800

    no message

--- buy.php
+++ buy.php
@@ -12,13 +12,13 @@ try {
     $sql = "update member set squid_qty = squid_qty - :furn_price where mem_no=:mem_no";
     $countMoney = $pdo->prepare($sql);
     $countMoney->bindValue(":furn_price", $_REQUEST["furn_price"]);
-    $countMoney->bindValue(":mem_no", $mem_no);
+    $countMoney->bindValue(":mem_no", $mem_no); //from session
     $countMoney->execute();
 
 
     $sql = "INSERT INTO `mem_furniture`(`mem_no`, `furn_no`, `pur_time`, `is_using`) VALUES (:mem_no,:furn_no,:pur_time,0)";
     $addFur = $pdo->prepare($sql);
-    $addFur->bindValue(":mem_no", $mem_no);
+    $addFur->bindValue(":mem_no", $mem_no); //from session
     $addFur->bindValue(":furn_no", $_REQUEST["furn_no"]);
     $addFur->bindValue(":pur_time", date("Y-m-d"));
     $addFur->execute();
@@ -26,7 +26,7 @@ try {
 
     $sql = "select * from mem_furniture where mem_no=:mem_no";
     $mem_furns = $pdo->prepare($sql);
-    $mem_furns->bindValue(":mem_no", $mem_no);
+    $mem_furns->bindValue(":mem_no", $mem_no); //from session
     $mem_furns->execute();
     $mem_furnsArr = array();
     while ($mem_furnRow = $mem_furns->fetchObject()) {
