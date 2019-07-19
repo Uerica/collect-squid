8b71ac6ea387fbc5be0b52720d9faedacf78a0df
theHardest <abcmattttt@gmail.com>
2019-07-19 09:52:52 +0800

    no message

--- buy.php
+++ buy.php
@@ -1,38 +1,44 @@
 <?php
-$dns = "mysql:host=sql.uerica.com;port=3307;dbname=dd101g2;charset=utf8";
-$user = "dd101g2";
-$psw = "dd101g2";
-$options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$pdo = new PDO($dns, $user, $psw, $options);
+$errMsg = "";
+try {
+    $dns = "mysql:host=sql.uerica.com;port=3307;dbname=dd101g2;charset=utf8";
+    $user = "dd101g2";
+    $psw = "dd101g2";
+    $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dns, $user, $psw, $options);
 
-$furn_price = $_REQUEST["furn_price"];
-$sql = "update member set squid_qty = squid_qty - :furn_price where mem_no=:mem_no";
-$countMoney = $pdo->prepare($sql);
-$countMoney->bindValue(":furn_price",$furn_price);
-$countMoney->bindValue(":mem_no","3"); //from session
-$countMoney->execute();
+    $mem_no = "1"; //from session
 
-
-$furn_no = $_REQUEST["furn_no"];
-$sql = "INSERT INTO `mem_furniture`(`mem_no`, `furn_no`, `pur_time`, `is_using`) VALUES (:mem_no,:furn_no,:pur_time,0)";
-$addFur = $pdo->prepare($sql);
-$addFur->bindValue(":mem_no", "3"); //from session
-$addFur->bindValue(":furn_no", $furn_no);
-$addFur->bindValue(":pur_time",date("Y-m-d"));
-$addFur->execute();
+    $sql = "update member set squid_qty = squid_qty - :furn_price where mem_no=:mem_no";
+    $countMoney = $pdo->prepare($sql);
+    $countMoney->bindValue(":furn_price", $_REQUEST["furn_price"]);
+    $countMoney->bindValue(":mem_no", $mem_no);
+    $countMoney->execute();
 
 
-$sql = "select * from mem_furniture where mem_no=:mem_no";
-$mem_furns = $pdo->prepare($sql);
-$mem_furns->bindValue(":mem_no", "3"); //from session
-$mem_furns->execute();
-$mem_furnsArr = array();
-while($mem_furnRow = $mem_furns->fetchObject()){
-    array_push($mem_furnsArr,$mem_furnRow->furn_no);
-}
-if(in_array($furn_no,$mem_furnsArr)){
-    echo "已購買";
-}else {
-    echo "購買";
+    $sql = "INSERT INTO `mem_furniture`(`mem_no`, `furn_no`, `pur_time`, `is_using`) VALUES (:mem_no,:furn_no,:pur_time,0)";
+    $addFur = $pdo->prepare($sql);
+    $addFur->bindValue(":mem_no", $mem_no);
+    $addFur->bindValue(":furn_no", $_REQUEST["furn_no"]);
+    $addFur->bindValue(":pur_time", date("Y-m-d"));
+    $addFur->execute();
+
+
+    $sql = "select * from mem_furniture where mem_no=:mem_no";
+    $mem_furns = $pdo->prepare($sql);
+    $mem_furns->bindValue(":mem_no", $mem_no);
+    $mem_furns->execute();
+    $mem_furnsArr = array();
+    while ($mem_furnRow = $mem_furns->fetchObject()) {
+        array_push($mem_furnsArr, $mem_furnRow->furn_no);
+    }
+    if (in_array($_REQUEST["furn_no"], $mem_furnsArr)) {
+        echo "已購買";
+    } else {
+        echo "購買";
+    }
+} catch (PDOException $e) {
+    echo "錯誤 : ", $e->getMessage(), "<br>";
+    echo "行號 : ", $e->getLine(), "<br>";
 }
 ?>
--- js/events.js
+++ js/events.js
@@ -1,130 +1,208 @@
 function init() {
-    show();
-    updateDetails(document.getElementById("button"));
-  
-    // RWD
-    window.addEventListener("resize", gameSizing);
-    menuMobileTransform();
-    regis();
-    cancelRegis();
-    confirmRegis();
-    cancelRaise();
-    confirmRaise();
-  
-    $(".myEvents .eventDescs").scroll(function() {
-      let s = $(this).scrollTop();
-      let h = $(this).height();
-      let ratio = (s / h) * 100;
-      let movePercent;
-      if (innerWidth < 737) {
-        movePercent = 10 + ratio / 10;
-      } else if (innerWidth < 1500) {
-        movePercent = 15 + ratio / 5;
-      } else {
-        movePercent = 15 + ratio / 3;
-      }
-      $(".rightHand").css({ top: `${movePercent}%` });
-    });
-  }
-  
-  // 一屏畫面 RWD
-  function gameSizing() {
-    let bodySize = [];
-    let htmlSize = [];
-    if (innerWidth / innerHeight > 1.75) {
-      bodySize[0] = document.body.clientWidth;
-      bodySize[1] = document.body.clientHeight;
-      htmlSize[0] = document.documentElement.clientWidth;
-      htmlSize[1] = document.documentElement.clientHeight;
-      querySize("body", htmlSize[0], htmlSize[1]);
+  show();
+  updateDetails(document.getElementById("button"));
+
+  // RWD
+  window.addEventListener("resize", gameSizing);
+  menuMobileTransform();
+  regis();
+  cancelRegis();
+  confirmRegis();
+  cancelRaise();
+  confirmRaise();
+
+  $(".myEvents .eventDescs").scroll(function () {
+    let s = $(this).scrollTop();
+    let h = $(this).height();
+    let ratio = (s / h) * 100;
+    let movePercent;
+    if (innerWidth < 737) {
+      movePercent = 10 + ratio / 10;
+    } else if (innerWidth < 1500) {
+      movePercent = 15 + ratio / 5;
     } else {
-      querySize("body", bodySize[0], bodySize[1]);
+      movePercent = 15 + ratio / 3;
     }
-  }
-  
-  function querySize(tag, w, h) {
-    document.querySelector(tag).style.width = w + "px";
-    document.querySelector(tag).style.height = h + "px";
-  }
-  
-  // 手機選單動畫
-  function menuMobileTransform() {
-    $(".menuMobile_link").click(function(e) {
-      e.preventDefault();
-  
-      $(".menuMobile_overlay").toggleClass("open");
-      $(".menuMobile").toggleClass("open");
+    $(".rightHand").css({
+      top: `${movePercent}%`
     });
-  }
-  
-  function regis() {
-    $(".eventsWrapper input").click(() =>
-      $(".regisBox").css({ display: "flex" })
-    );
-  }
-  
-  function cancelRegis() {
-    $(".regisBox .cancelBtn").click(() =>
-      $(".regisBox").css({ display: "none" })
-    );
-  }
-  
-  function confirmRegis() {
-    $(".regisBox input").click(() => {
-      $(".regisBox").css({ display: "none" });
-    });
-  }
-  
-  function cancelRaise() {
-    $(".raiseBox .cancelBtn").click(() => {
-      $(".raiseBox").css({ display: "none" });
-    });
-  }
-  
-  function confirmRaise() {
-    $(".raiseBox .submitWrapper input").click(() => {
-      console.log($(this));
-      $(".raiseBox").css({ display: "none" });
-    });
-  }
-  
-  // 螢幕橫向
-  function fullScreenCheck() {
-    if (document.fullscreenElement) return;
-    return document.documentElement.requestFullscreen();
-  }
-  
-  function updateDetails(lockButton) {
-    const buttonOrientation = getOppositeOrientation();
-    lockButton.textContent = `Lock to ${buttonOrientation}`;
-  }
-  
-  function getOppositeOrientation() {
-    const { type } = screen.orientation;
-    return type.startsWith("portrait") ? "landscape" : "portrait";
-  }
-  
-  async function rotate(lockButton) {
-    try {
-      await fullScreenCheck();
-    } catch (err) {
-      console.error(err);
-    }
-    const newOrientation = getOppositeOrientation();
-    await screen.orientation.lock(newOrientation);
-    updateDetails(lockButton);
-  }
-  
-  function show() {
-    const { type, angle } = screen.orientation;
-    console.log(`Orientation type is ${type} & angle is ${angle}.`);
-  }
-  
-  screen.orientation.addEventListener("change", () => {
-    show();
-    updateDetails(document.getElementById("button"));
   });
-  // 螢幕橫向
-  
-  window.onload = init;
-  
+}
+
+// 一屏畫面 RWD
+function gameSizing() {
+  let bodySize = [];
+  let htmlSize = [];
+  if (innerWidth / innerHeight > 1.75) {
+    bodySize[0] = document.body.clientWidth;
+    bodySize[1] = document.body.clientHeight;
+    htmlSize[0] = document.documentElement.clientWidth;
+    htmlSize[1] = document.documentElement.clientHeight;
+    querySize("body", htmlSize[0], htmlSize[1]);
+  } else {
+    querySize("body", bodySize[0], bodySize[1]);
+  }
+}
+
+function querySize(tag, w, h) {
+  document.querySelector(tag).style.width = w + "px";
+  document.querySelector(tag).style.height = h + "px";
+}
+
+// 手機選單動畫
+function menuMobileTransform() {
+  $(".menuMobile_link").click(function (e) {
+    e.preventDefault();
+
+    $(".menuMobile_overlay").toggleClass("open");
+    $(".menuMobile").toggleClass("open");
+  });
+}
+
+function regis() {
+  $(".eventsWrapper input").click(() =>
+    $(".regisBox").css({
+      display: "flex"
+    })
+  );
+}
+
+function cancelRegis() {
+  $(".regisBox .cancelBtn").click(() =>
+    $(".regisBox").css({
+      display: "none"
+    })
+  );
+}
+
+function confirmRegis() {
+  $(".regisBox input").click(() => {
+    $(".regisBox").css({
+      display: "none"
+    });
+  });
+}
+
+function cancelRaise() {
+  $(".raiseBox .cancelBtn").click(() => {
+    $(".raiseBox").css({
+      display: "none"
+    });
+  });
+}
+
+function confirmRaise() {
+  $(".raiseBox .submitWrapper input").click(() => {
+    console.log($(this));
+    $(".raiseBox").css({
+      display: "none"
+    });
+  });
+}
+
+// 螢幕橫向
+function fullScreenCheck() {
+  if (document.fullscreenElement) return;
+  return document.documentElement.requestFullscreen();
+}
+
+function updateDetails(lockButton) {
+  const buttonOrientation = getOppositeOrientation();
+  lockButton.textContent = `Lock to ${buttonOrientation}`;
+}
+
+function getOppositeOrientation() {
+  const {
+    type
+  } = screen.orientation;
+  return type.startsWith("portrait") ? "landscape" : "portrait";
+}
+
+async function rotate(lockButton) {
+  try {
+    await fullScreenCheck();
+  } catch (err) {
+    console.error(err);
+  }
+  const newOrientation = getOppositeOrientation();
+  await screen.orientation.lock(newOrientation);
+  updateDetails(lockButton);
+}
+
+function show() {
+  const {
+    type,
+    angle
+  } = screen.orientation;
+  console.log(`Orientation type is ${type} & angle is ${angle}.`);
+}
+
+screen.orientation.addEventListener("change", () => {
+  show();
+  updateDetails(document.getElementById("button"));
+});
+// 螢幕橫向
+
+window.onload = init;
+
+
+// addEvent----------------------
+$(document).ready(function () {
+  $("#evt_init").click(function () {
+
+    if ($("#evt_name").val() == "") {
+      alert("evt_name必填");
+      return;
+    }
+
+    if ($("#evt_date").val() == "" || $("#evt_date").val() < $("#enroll_end_date").val()) {
+      alert("evt_date必填且不能大於enroll_end_date");
+      return;
+    }
+
+    if ($("#enroll_end_date").val() == "" || $("#enroll_end_date").val() > $("#evt_date").val()) {
+      alert("enroll_end_date必填且不能小於evt_date");
+      return;
+    }
+
+    if ($("#min_att").val() == "" || $("#min_att").val() < 3) {
+      alert("min_att必填且不能小於3");
+      return;
+    }
+
+    if ($("#max_att") == "" || $("#max_att") > 30) {
+      alert("max_att必填且不能大於30");
+      return;
+    }
+
+    if ($("#evt_place") == "") {
+      alert("evt_place必填");
+      return;
+    }
+
+    if ($("#evt_desc") == "") {
+      alert("evt_desc必填");
+      return;
+    }
+
+    if ($("#evt_trans_info") == "") {
+      alert("evt_trans_info必填");
+      return;
+    }
+
+    // let xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function () {
+    //   if (xhr.readyState == 4) {
+    //     if (xhr.status == 200) {
+    //       console.log(xhr.responseText);
+    //     } else {
+    //       alert(xhr.status);
+    //     }
+    //   }
+    // }
+    // let url = "addEvent.php";
+    // xhr.open("post", url, true);
+    // let addEvtForm = new FormData(this.parentNode);
+    // xhr.send(addEvtForm);
+  });
+});
