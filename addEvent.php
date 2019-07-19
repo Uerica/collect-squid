2599a210e59ac9c47945428512a8e032bd3652aa
theHardest <abcmattttt@gmail.com>
2019-07-19 15:40:35 +0800

    no message

--- addEvent.php
+++ addEvent.php
@@ -7,19 +7,17 @@ try {
     $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     $pdo = new PDO($dns, $user, $psw, $options);
 
-    $sql = "INSERT INTO `event` (`evt_no`, `org_mem_no`, `evt_name`, `evt_date`, `enroll_end_date`, `min_att`, `max_att`, `evt_place`, `evt_desc`, `evt_trans_info`, `evt_cover_url`) VALUES (NULL, :mem_no, :evt_name, :evt_date, :enroll_end_date, :min_att, :max_att, :evt_place, :evt_desc, :evt_trans_info, :evt_cover_url)";
+    $sql = "INSERT INTO `event` (`evt_no`, `org_mem_no`, `evt_name`, `evt_date`, `enroll_end_date`, `max_att`, `evt_place`, `evt_desc`, `evt_cover_url`) VALUES (NULL, :mem_no, :evt_name, :evt_date, :enroll_end_date, :max_att, :evt_place, :evt_desc, :evt_cover_url)";
 
     $evtAdd = $pdo->prepare($sql);
     $evtAdd->bindValue(":mem_no", "1"); //from session
     $evtAdd->bindValue(":evt_name", $_REQUEST["evt_name"]);
     $evtAdd->bindValue(":evt_date", $_REQUEST["evt_date"]);
     $evtAdd->bindValue(":enroll_end_date", $_REQUEST["enroll_end_date"]);
-    $evtAdd->bindValue(":min_att", $_REQUEST["min_att"]);
     $evtAdd->bindValue(":max_att", $_REQUEST["max_att"]);
     $evtAdd->bindValue(":evt_place", $_REQUEST["evt_place"]);
     $evtAdd->bindValue(":evt_desc", $_REQUEST["evt_desc"]);
-    $evtAdd->bindValue(":evt_trans_info", $_REQUEST["evt_trans_info"]);
-    $evtAdd->bindValue(":evt_cover_url", $_REQUEST["evt_cover_url"]);
+    $evtAdd->bindValue(":evt_cover_url", '$_REQUEST["evt_cover_url"]');
     $evtAdd->execute();
 
 } catch (PDOException $e) {
--- js/events.js
+++ js/events.js
@@ -151,58 +151,51 @@ $(document).ready(function () {
   $("#evt_init").click(function () {
 
     if ($("#evt_name").val() == "") {
-      alert("evt_name必填");
+      alert("活動名稱必填");
       return;
     }
 
     if ($("#evt_date").val() == "" || $("#evt_date").val() < $("#enroll_end_date").val()) {
-      alert("evt_date必填且不能大於enroll_end_date");
+      alert("活動時間必填且不能在報名截止時間之前");
+      return;
+    }
+
+    if ($("#evt_place").val() == "") {
+      alert("活動地點必填");
       return;
     }
 
     if ($("#enroll_end_date").val() == "" || $("#enroll_end_date").val() > $("#evt_date").val()) {
-      alert("enroll_end_date必填且不能小於evt_date");
+      alert("報名截止時間必填且不能在活動時間之後");
       return;
     }
 
-    if ($("#min_att").val() == "" || $("#min_att").val() < 3) {
-      alert("min_att必填且不能小於3");
+    if ($("#max_att").val() == "") {
+      alert("人數限制必填3~20人");
+      return;
+    } else if ($("#max_att").val() < 3 || $("#max_att").val() > 20) {
+      alert("人數限制必填3~20人");
       return;
     }
 
-    if ($("#max_att") == "" || $("#max_att") > 30) {
-      alert("max_att必填且不能大於30");
+    if ($("#evt_desc").val() == "") {
+      alert("活動描述必填");
       return;
+    } else {
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            alert("舉辦活動成功");
+          } else {
+            alert(xhr.status);
+          }
+        }
+      }
+      let url = "addEvent.php";
+      xhr.open("post", url, true);
+      let addEvtForm = new FormData(document.getElementById("addEvtForm"));
+      xhr.send(addEvtForm);
     }
-
-    if ($("#evt_place") == "") {
-      alert("evt_place必填");
-      return;
-    }
-
-    if ($("#evt_desc") == "") {
-      alert("evt_desc必填");
-      return;
-    }
-
-    if ($("#evt_trans_info") == "") {
-      alert("evt_trans_info必填");
-      return;
-    }
-
-    // let xhr = new XMLHttpRequest();
-    // xhr.onreadystatechange = function () {
-    //   if (xhr.readyState == 4) {
-    //     if (xhr.status == 200) {
-    //       console.log(xhr.responseText);
-    //     } else {
-    //       alert(xhr.status);
-    //     }
-    //   }
-    // }
-    // let url = "addEvent.php";
-    // xhr.open("post", url, true);
-    // let addEvtForm = new FormData(this.parentNode);
-    // xhr.send(addEvtForm);
   });
 });
