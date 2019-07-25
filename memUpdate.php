08023bc52ebd77f4b1c87b547e41d5597d53e1ce
ak190527 <kungpop1107@gmail.com>
2019-07-25 17:25:54 +0800

    增加session_start(), 抓mem_no用session[ ]的方式.

--- memUpdate.php
+++ memUpdate.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 $errMsg = "";
 
 try {
 	require_once("connectDB.php");
 	
-	$sql = "update member set mem_name=:mem_name,mem_gender=:mem_gender,mem_dob=:mem_dob,mem_sign=:mem_sign,mem_pwd=:mem_pwd,mem_email=:mem_email where mem_no = :mem_no";//加where + 欄位
+	$sql = "UPDATE member set mem_name=:mem_name,mem_gender=:mem_gender,mem_dob=:mem_dob,mem_sign=:mem_sign,mem_pwd=:mem_pwd,mem_email=:mem_email where mem_no = :mem_no";//加where + 欄位
     $members = $pdo->prepare($sql); 
     $members->bindValue(":mem_email",$_REQUEST["mem_email"]);
 	$members->bindValue(":mem_name",$_REQUEST["mem_name"]);
@@ -12,8 +13,8 @@ try {
 	$members->bindValue(":mem_dob",$_REQUEST["mem_dob"]);
 	$members->bindValue(":mem_sign",$_REQUEST["mem_sign"]);
 	$members->bindValue(":mem_pwd",$_REQUEST["mem_pwd"]);
-	$members->bindValue(":mem_no",111);//寫session時改
-    $members->execute();
+	$members->bindValue(":mem_no",$_SESSION["mem_no"]);//寫session時改
+	$members->execute();
 
 } catch (PDOException $e) {
 	echo "錯誤 : ", $e -> getMessage(), "<br>";
